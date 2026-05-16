8866641ce13971d90f847394fcd956b45e7e50ff
Shubham kesharwani7223 <er.shubham7223@gmail.com>
2026-05-16 20:54:33 +0530

    small change

--- frontend/src/components/Admin/Timeline.jsx
+++ frontend/src/components/Admin/Timeline.jsx
@@ -1,287 +1,58 @@
-// import React, { useState, useEffect } from "react";
-// import { useAlert } from "react-alert";
-// import { useDispatch, useSelector } from "react-redux";
-// import { addTimeline, deleteTimeline, getUser } from "../../actions/user";
-// import { MdKeyboardBackspace } from "react-icons/md";
-// import { Button, Typography } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import { FaTrash } from "react-icons/fa";
-
-// const Timeline = () => {
-//   const { message, error, loading } = useSelector((state) => state.update);
-//   const { message: loginMessage } = useSelector((state) => state.login);
-//   const { user } = useSelector((state) => state.user);
-
-//   const dispatch = useDispatch();
-//   const alert = useAlert();
-
-//   const [title, setTitle] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [date, setDate] = useState("");
-
-//   const submitHandler = async (e) => {
-//     e.preventDefault();
-//     await dispatch(addTimeline(title, description, date));
-//     dispatch(getUser());
-//   };
-//   const deleteHandler = async (id) => {
-//     await dispatch(deleteTimeline(id));
-//     dispatch(getUser());
-//   };
-
-//   useEffect(() => {
-//     if (error) {
-//       alert.error(error);
-//       dispatch({ type: "CLEAR_ERRORS" });
-//     }
-//     if (message) {
-//       alert.success(message);
-//       dispatch({ type: "CLEAR_MESSAGE" });
-//     }
-
-//     if (loginMessage) {
-//       alert.success(loginMessage);
-//       dispatch({ type: "CLEAR_MESSAGE" });
-//     }
-//   }, [alert, error, message, dispatch, loginMessage]);
-
-//   return (
-//     <div className="adminPanel">
-//       <div className="adminPanelContainer">
-//         <Typography variant="h4">
-//           <p>A</p>
-//           <p>D</p>
-//           <p>M</p>
-//           <p>I</p>
-//           <p style={{ marginRight: "1vmax" }}>N</p>
-
-//           <p>P</p>
-//           <p>A</p>
-//           <p>N</p>
-//           <p>E</p>
-//           <p>L</p>
-//         </Typography>
-
-//         <form onSubmit={submitHandler}>
-//           <input
-//             type="text"
-//             placeholder="Title"
-//             value={title}
-//             onChange={(e) => setTitle(e.target.value)}
-//             className="adminPanelInputs"
-//           />
-//           <input
-//             type="text"
-//             placeholder="Description"
-//             value={description}
-//             onChange={(e) => setDescription(e.target.value)}
-//             className="adminPanelInputs"
-//           />
-//           <input
-//             type="date"
-//             placeholder="Date"
-//             value={date}
-//             onChange={(e) => setDate(e.target.value)}
-//             className="adminPanelInputs"
-//           />
-
-//           <Link to="/account">
-//             BACK <MdKeyboardBackspace />
-//           </Link>
-
-//           <Button type="submit" variant="contained" disabled={loading}>
-//             Add
-//           </Button>
-//         </form>
-
-//         <div className="adminPanelYoutubeVideos">
-//           {user &&
-//             user.timeline &&
-//             user.timeline.map((item) => (
-//               <div className="youtubeCard" key={item._id}>
-//                 <Typography variant="h6">{item.title}</Typography>
-//                 <Typography variant="body1" style={{ letterSpacing: "2px" }}>
-//                   {item.description}
-//                 </Typography>
-//                 <Typography variant="body1" style={{ fontWeight: 600 }}>
-//                   {item.date.toString().split("T")[0]}
-//                 </Typography>
-
-//                 <Button
-//                   style={{
-//                     margin: "auto",
-//                     display: "block",
-//                     color: "rgba(40,40,40,0.7)",
-//                   }}
-//                   onClick={() => deleteHandler(item._id)}
-//                 >
-//                   <FaTrash />
-//                 </Button>
-//               </div>
-//             ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Timeline;
-
-
-
-
-import React, { useState, useEffect } from "react";
-import { useAlert } from "react-alert";
-import { useDispatch, useSelector } from "react-redux";
-import { addTimeline, deleteTimeline, getUser } from "../../actions/user";
-import { MdKeyboardBackspace } from "react-icons/md";
-import { Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-import { FaTrash } from "react-icons/fa";
+import React from "react";
+import "./Timeline.css";
+import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import MuiTimeline from "@mui/lab/Timeline";
 
 const Timeline = () => {
-  const { message, error, loading } = useSelector((state) => state.update);
-  const { message: loginMessage } = useSelector((state) => state.login);
   const { user } = useSelector((state) => state.user);
 
-  const dispatch = useDispatch();
-  const alert = useAlert();
-
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState("");
-
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    await dispatch(addTimeline(title, description, date));
-    dispatch(getUser());
-
-    setTitle("");
-    setDescription("");
-    setDate("");
-  };
-
-  const deleteHandler = async (id) => {
-    await dispatch(deleteTimeline(id));
-    dispatch(getUser());
-  };
-
-  useEffect(() => {
-    if (error) {
-      alert.error(error);
-      dispatch({ type: "CLEAR_ERRORS" });
-    }
-
-    if (message) {
-      alert.success(message);
-      dispatch({ type: "CLEAR_MESSAGE" });
-    }
-
-    if (loginMessage) {
-      alert.success(loginMessage);
-      dispatch({ type: "CLEAR_MESSAGE" });
-    }
-  }, [alert, error, message, dispatch, loginMessage]);
-
   return (
-    <div className="adminPanel">
-      <div className="adminPanelContainer">
-        <Typography variant="h4">
-          <p>A</p>
-          <p>D</p>
-          <p>M</p>
-          <p>I</p>
-          <p style={{ marginRight: "1vmax" }}>N</p>
+    <div className="timeline">
+      <Typography variant="h3">TIMELINE</Typography>
 
-          <p>P</p>
-          <p>A</p>
-          <p>N</p>
-          <p>E</p>
-          <p>L</p>
-        </Typography>
+      <MuiTimeline align="alternate">
+        {user &&
+          user.timeline &&
+          user.timeline.map((item, index) => (
+            <TimelineItem key={item._id || index}>
+              <TimelineOppositeContent color="textSecondary">
+                {new Date(item.date).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </TimelineOppositeContent>
 
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="adminPanelInputs"
-            required
-          />
+              <TimelineSeparator>
+                <TimelineDot color="primary">
+                  <LaptopMacIcon />
+                </TimelineDot>
 
-          <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="adminPanelInputs"
-            required
-          />
+                {index !== user.timeline.length - 1 && (
+                  <TimelineConnector />
+                )}
+              </TimelineSeparator>
 
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="adminPanelInputs"
-            required
-          />
-
-          <Link to="/account">
-            BACK <MdKeyboardBackspace />
-          </Link>
-
-          <Button type="submit" variant="contained" disabled={loading}>
-            ADD
-          </Button>
-        </form>
-
-        <div className="adminPanelYoutubeVideos">
-          {user &&
-            user.timeline &&
-            user.timeline.map((item) => (
-              <div className="youtubeCard" key={item._id}>
-                <Typography variant="h6">
+              <TimelineContent>
+                <Typography variant="h5">
                   {item.title}
                 </Typography>
 
-                <Typography
-                  variant="body1"
-                  style={{ letterSpacing: "1px" }}
-                >
+                <Typography>
                   {item.description}
                 </Typography>
-
-                <Typography
-                  variant="body1"
-                  style={{
-                    fontWeight: 600,
-                    marginTop: "10px",
-                    color: "gray",
-                  }}
-                >
-                  {new Date(item.date).toLocaleDateString("en-US", {
-                    day: "numeric",
-                    month: "short",
-                    year: "numeric",
-                  })}
-                </Typography>
-
-                <Button
-                  style={{
-                    margin: "auto",
-                    display: "block",
-                    color: "rgba(40,40,40,0.7)",
-                  }}
-                  onClick={() => deleteHandler(item._id)}
-                >
-                  <FaTrash />
-                </Button>
-              </div>
-            ))}
-        </div>
-      </div>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+      </MuiTimeline>
     </div>
   );
 };
